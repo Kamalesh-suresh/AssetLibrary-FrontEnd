6ccd6e1c872219b06ed17fa08a2d38d5d69bfdc1
kamalesh psiog <kamalesh.suresh@psiog.com>
2025-12-29 12:06:01 +0530

    Added signup UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Login } from './container/login/login';
+import { Signup } from './container/signup/signup';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: Login,
+  },
+  {
+    path: 'signup',
+    component: Signup,
   },
 ];
---
+++ src/app/container/signup/signup.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-signup',
+  imports: [ReactiveFormsModule, InputTextModule, PasswordModule, ButtonModule],
+  templateUrl: './signup.html',
+  styleUrl: './signup.css',
+})
+export class Signup {
+  signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.signupForm.invalid) {
+      this.signupForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Signup Data:', this.signupForm.value);
+    // Call API here
+  }
+}
