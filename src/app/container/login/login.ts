3a02a428796ef0a218ddd584567e422e237fc009
kamalesh psiog <kamalesh.suresh@psiog.com>
2025-12-29 12:27:34 +0530

    Added login page to UI

--- src/app/container/login/login.ts
+++ src/app/container/login/login.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.html',
-  imports: [],
+  imports: [ReactiveFormsModule, InputTextModule, PasswordModule, ButtonModule, RouterModule],
 })
-export class Login {}
+export class Login {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Login Data:', this.loginForm.value);
+    // Call login API here
+  }
+}
